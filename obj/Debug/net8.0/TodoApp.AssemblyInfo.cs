87b16b77a24b51adf041cd8a8dd9adf7fe0a5d9c
Willikox <willedu94@gmail.com>
2024-09-12 16:23:20 -0500

    ignore folder auto

--- obj/Debug/net8.0/TodoApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faeea2e431ad4925a9a5b6485d0ffaa3459bd4c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
